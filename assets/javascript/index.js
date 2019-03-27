b231c264b648fe1f160f1e26bb05145347cb5d75
OZKAN <alper.ozkan.p@gmail.com>
2019-03-27 16:15:47 +0100

    Autocomplete fait

---
+++ assets/javascript/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // On utilise autocomplete afin de proposer automatiquement des suggestions pour l'input de l'utilisateur
+    $("#nomCommune").autocomplete({
+        source: function (req, response) {
+            fetch("https://geo.api.gouv.fr/communes?nom=" + $("#nomCommune").val() + "&fields=departement&boost=population")
+                .then(
+                    function (responseFetch) {
+                        // Si on ne recoit rien
+                        if (responseFetch.status !== 200) {
+                            console.log("Probleme avec la requete pour trouver le nom de commune. Code de status : " + response.status);
+                            return;
+                        }
+
+                        // On fait des choses avec les données reçues
+                        responseFetch.json().then(
+                            function (data) {
+                                // Ici on recupere le nom de la commune
+                                const resultat = data.map( function (item) {
+                                    return item.nom;
+                                });
+                                response(resultat);
+                            });
+                    }
+                )
+                .catch(function (err) {
+                    console.log("Erreur de Fetch: ", err);
+                });
+        },
+        minLength:3 // Le nombre minimum de caracteres a taper avant que les suggestions apparaissent
+    })
+});
