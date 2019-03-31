444a5898a4b938c0abe3fe6cd142afc70c19fa63
OZKAN <alper.ozkan.p@gmail.com>
2019-03-31 20:44:15 +0200

    DatePicker

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -13,10 +13,14 @@ $(document).ready(function () {
         active: 0
     });
 
+    // La date que l'on peut choisir
+    $( "#datepicker" ).datepicker();
+
     // Au clic sur l'icone
     $("#boutonRecherche").click(function () {
         mesImages = new Images(); // Une liste d'images qui contiendra les images qu'on recupère du fetch
         $("#photo").empty(); // On vide les images avant d'en ajouter de nouvelles
+
 
 
         // On retrouve les photos
@@ -32,21 +36,34 @@ $(document).ready(function () {
                 }
 
                 response.json().then(function (data) {
-
-
-
                     let i = 0;
                     // Nous parcourons le resultat de la requete pour recuperer les infos dont nous avons besoins
                     data.photos.photo.forEach(function (photo) {
-
-
                         // Nous gardons les images en mémoire pour utilisation plus tard
                         let monImage = new Image(photo.title, photo.farm, photo.server, photo.id, photo.secret);
 
-                        // Pour recuperer d'autres informations a propos de photos
-                        Fetch.getInfoPhoto(monImage.getId(), monImage.getSecret()).then(function (data) {
+                        // Pour recuperer d'autres informations en plus a propos de photos et les ajouter dans les images
+                        Fetch.getInfoPhoto(monImage.getId(), monImage.getSecret()).then((data) => {
                             monImage.setTimestamp(data.photo.dates.taken);
                             monImage.setAuthor(data.photo.owner.username);
+                            mesImages.add(monImage);
+
+                        }).then(() => {
+                            console.log(1, monImage.getTimestamp());
+
+                            // Datatables ici car on veut avoir des infos une fois que les photos seront venues
+                            let t = $('#example').DataTable();
+                            mesImages.getEveryImage().forEach((element) => {
+                                t.row.add([
+                                    element.getTitle(),
+                                    element.getAuthor(),
+                                    element.getTimestamp(),
+                                    element.getFarmId(),
+                                    element.getServerId(),
+                                    element.getId(),
+                                    element.getSecret()
+                                ]).draw(false);
+                            });
                         });
 
                         // Nous ajoutons les images dans la page.
@@ -54,7 +71,7 @@ $(document).ready(function () {
 
                         // La boite de dialogue
                         $("#dialogImg").dialog({autoOpen: false});
-                        $("#image" + i).click(function () {
+                        $("#image" + i).click(function () { // Au clic d'une image
                             $("#dialogImg").dialog("open")
                                 .dialog({
                                     title: monImage.getTitle()
@@ -62,30 +79,16 @@ $(document).ready(function () {
                                 .empty()
                                 // Ici on rajoute des infos sur la boite de dialogue
                                 .append("<div>Timestamp de prise de vue: " + monImage.getTimestamp() + "</div>")
-                                .append("<div>Pseudo de l'uploader : " + monImage.getAuthor() + "</div>");;
-
+                                .append("<div>Pseudo de l'uploader : " + monImage.getAuthor() + "</div>");
                         });
-                        mesImages.add(monImage);
                         i++;
                     });
-
-                    // Datatables ici car on veut avoir des infos une fois que les photos seront venues
-                    let t = $('#example').DataTable();
-                    mesImages.getEveryImage().forEach((element) => {
-                        t.row.add([
-                            element.getTitle(),
-                            element.getFarmId(),
-                            element.getServerId(),
-                            element.getId(),
-                            element.getSecret()
-                        ]).draw(false);
-                    });
-
                 })
             })
             .catch(function (err) {
                 console.log("Erreur de Fetch sur la recherche et l'affichage d'image");
             });
+
 
     });
 
