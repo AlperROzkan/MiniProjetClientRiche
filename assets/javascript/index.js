665aeecb78ab6e95c7cde0367940053431a7825d
OZKAN <alper.ozkan.p@gmail.com>
2019-04-07 22:19:00 +0200

    Fix vidage des tables meme après requete vide Dialogue si requete ne donne rien

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -21,8 +21,16 @@ $(document).ready(function () {
         }
     });
 
-    // Au clic sur l'icone
+    // Au clic sur l'icone de la loupe
     $("#boutonRecherche").click(function () {
+        // Redirection vers la vue photo pour une nouvelle rechereche
+        $("#vues").tabs({
+            active: 0
+        });
+
+        // On initialise une datatables
+        let t = $('#datatable').show().DataTable();
+
         mesImages = new Images(); // Une liste d'images qui contiendra les images qu'on recupère du fetch
         $("#photo").empty(); // On vide les images avant d'en ajouter de nouvelles
         // On retrouve les photos
@@ -40,54 +48,73 @@ $(document).ready(function () {
                 }
 
                 response.json().then(function (data) {
-                    let i = 0;
-                    // Nous parcourons le resultat de la requete pour recuperer les infos dont nous avons besoins
-                    data.photos.photo.forEach(function (photo) {
-                        // Nous gardons les images en mémoire pour utilisation plus tard
-                        let monImage = new Image(photo.title, photo.farm, photo.server, photo.id, photo.secret);
+                    // Initialisation de la boite de dialogue
+                    $("#dialogImg").dialog({autoOpen: false});
 
-                        // Pour recuperer d'autres informations en plus a propos de photos et les ajouter dans les images (classe Fetch)
-                        Fetch.getInfoPhoto(monImage.getId(), monImage.getSecret()).then((data) => {
-                            monImage.setTimestamp(data.photo.dates.taken);
-                            monImage.setAuthor(data.photo.owner.username);
-                            mesImages.add(monImage);
+                    // On vide le contenu de la datatable
+                    $("#bodyTable").empty();
 
-                        }).then(() => {
-                            // Datatables ici car on veut avoir des infos une fois que les photos seront venues
-                            let t = $('#datatable').show().DataTable();
+                    // Si il n'y a pas d'image pour une requete
+                    if (data.photos.total === "0") {
 
-                            t.clear();
-                            mesImages.getEveryImage().forEach((element) => {
-                                // On remplit les tables avec des infos de chaque photo
-                                t.row.add([
-                                    element.getTitle(),
-                                    element.getAuthor(),
-                                    element.getTimestamp(),
-                                    element.getFarmId(),
-                                    element.getServerId(),
-                                    element.getId(),
-                                    element.getSecret()
-                                ]).draw(false);
+                        $("#dialogImg").dialog("open")
+                            .dialog({
+                                title: "Pas d'image"
+                            })
+                            .empty()
+                            // Ici on rajoute des infos sur la boite de dialogue
+                            .append("<div>Pas d'image pour cette requete !");
+                    } else {
+                        let i = 0;
+                        // Nous parcourons le resultat de la requete pour recuperer les infos dont nous avons besoins
+                        data.photos.photo.forEach(function (photo) {
+                            // Nous gardons les images en mémoire pour utilisation plus tard
+                            let monImage = new Image(photo.title, photo.farm, photo.server, photo.id, photo.secret);
+
+                            // Pour recuperer d'autres informations en plus a propos de photos et les ajouter dans les images (classe Fetch)
+                            Fetch.getInfoPhoto(monImage.getId(), monImage.getSecret()).then((data) => {
+                                monImage.setTimestamp(data.photo.dates.taken);
+                                monImage.setAuthor(data.photo.owner.username);
+                                mesImages.add(monImage);
+
+                            }).then(() => {
+                                // Datatables ici car on veut avoir des infos une fois que les photos seront venues
+                                t = $('#datatable').show().DataTable();
+
+                                t.clear();
+                                mesImages.getEveryImage().forEach((element) => {
+                                    // On remplit les tables avec des infos de chaque photo
+                                    t.row.add([
+                                        element.getTitle(),
+                                        element.getAuthor(),
+                                        element.getTimestamp(),
+                                        element.getFarmId(),
+                                        element.getServerId(),
+                                        element.getId(),
+                                        element.getSecret()
+                                    ]).draw(false);
+                                });
                             });
-                        });
 
-                        // Nous ajoutons les images dans la page.
-                        $("#photo").append("<img alt=imageRequete" + i + " id=image" + i + " src=" + monImage.getUrl() + "/>");
+                            // Nous ajoutons les images dans la page.
+                            $("#photo").append("<img alt=imageRequete" + i + " id=image" + i + " src=" + monImage.getUrl() + "/>");
 
-                        // La boite de dialogue
-                        $("#dialogImg").dialog({autoOpen: false});
-                        $("#image" + i).click(function () { // Au clic d'une image
-                            $("#dialogImg").dialog("open")
-                                .dialog({
-                                    title: monImage.getTitle()
-                                })
-                                .empty()
-                                // Ici on rajoute des infos sur la boite de dialogue
-                                .append("<div>Timestamp de prise de vue: " + monImage.getTimestamp() + "</div>")
-                                .append("<div>Pseudo de l'uploader : " + monImage.getAuthor() + "</div>");
+                            // On affiche des infos au clic sur une image
+                            $("#image" + i).click(function () { // Au clic d'une image
+                                $("#dialogImg").dialog("open")
+                                    .dialog({
+                                        title: monImage.getTitle()
+                                    })
+                                    .empty()
+                                    // Ici on rajoute des infos sur la boite de dialogue
+                                    .append("<div>Timestamp de prise de vue: " + monImage.getTimestamp() + "</div>")
+                                    .append("<div>Pseudo de l'uploader : " + monImage.getAuthor() + "</div>");
+                            });
+                            i++;
                         });
-                        i++;
-                    });
+                    }
+
+
                 })
             })
             .catch(function (err) {
